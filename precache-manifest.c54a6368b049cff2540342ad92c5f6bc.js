0c96ce03b48a981eefad72e6b09933c8364e4b74
Matei Mircea <mrc_matei@yahoo.com>
2020-10-24 18:56:00 +0300

    Updates

---
+++ precache-manifest.c54a6368b049cff2540342ad92c5f6bc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8fdef986634c3967cef2ea50982a0d",
+    "url": "/movie-cloud/index.html"
+  },
+  {
+    "revision": "a4d96b22e16d6910b87b",
+    "url": "/movie-cloud/static/css/2.855234ec.chunk.css"
+  },
+  {
+    "revision": "58302022de8fc96bf297",
+    "url": "/movie-cloud/static/css/main.6b6880f9.chunk.css"
+  },
+  {
+    "revision": "a4d96b22e16d6910b87b",
+    "url": "/movie-cloud/static/js/2.e9506a8f.chunk.js"
+  },
+  {
+    "revision": "6c88e50fd546ef59bde03720d9766cbb",
+    "url": "/movie-cloud/static/js/2.e9506a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58302022de8fc96bf297",
+    "url": "/movie-cloud/static/js/main.d58a1282.chunk.js"
+  },
+  {
+    "revision": "9a2133cb4159a6ca3c25",
+    "url": "/movie-cloud/static/js/runtime-main.e9c49ca6.js"
+  },
+  {
+    "revision": "0b22b9a5068c0135ead1f86afa8f912d",
+    "url": "/movie-cloud/static/media/moviecloudresized.0b22b9a5.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/movie-cloud/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/movie-cloud/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/movie-cloud/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/movie-cloud/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c95ac78ea87d34111517f24db5335948",
+    "url": "/movie-cloud/static/media/theatre.c95ac78e.jpg"
+  }
+]);
