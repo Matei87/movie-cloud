93e0811f3ec56d9d07d8dcf83331ed956f15a212
Matei Mircea <mrc_matei@yahoo.com>
2020-12-12 01:19:25 +0200

    small bootstrap fix on app.js

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import Footer from './components/Footer/Footer';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 
 //import Homepage from './components/Homepage/Homepage';
