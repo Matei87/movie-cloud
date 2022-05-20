7bc0b1dfe522388b8d6b4233f7f88dfe988f7b14
Matei87 <mrc_matei@yahoo.com>
2022-05-20 19:37:40 +0300

    Updates

---
+++ static/js/617.6fa65a1b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmovie_cloud=self.webpackChunkmovie_cloud||[]).push([[617],{8617:function(e,t,l){l.d(t,{QCV:function(){return n}});var u=l(9983);function n(e){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 20 20",fill:"currentColor"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z",clipRule:"evenodd"}}]})(e)}}}]);
+//# sourceMappingURL=617.6fa65a1b.chunk.js.map
